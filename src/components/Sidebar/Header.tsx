c9cca9386d47fee4675154ff9df533df216f4e3d
Ruver Dornelas <ruverd@gmail.com>
2021-04-09 13:21:31 -0400

    bugfix: Misspell image name

--- src/components/Sidebar/Header.tsx
+++ src/components/Sidebar/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Header(){
   return (
     <Box align="center" pad={{ vertical: 'medium' }} >
-      <Image src="/Logo.svg" layout='fixed' width="140" height="50" /> 
+      <Image src="/logo.svg" layout='fixed' width="140" height="50" /> 
     </Box>    
   );
 }
