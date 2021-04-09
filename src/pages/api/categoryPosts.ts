bc9a303a57687f29cfd71ca2db7aec251bd9e578
Ruver Dornelas <ruverd@gmail.com>
2021-04-09 13:54:22 -0400

    chore: Remove console log

--- src/pages/api/categoryPosts.ts
+++ src/pages/api/categoryPosts.ts
@@ -4,7 +4,6 @@ export default async function categories(req: NextApiRequest, res: NextApiRespon
   const { category } = req.query;
 
 	const url = `https://reddit.com/r/${category}.json`;
-	console.log(url);
 
 	const posts = await fetch(url).then((res) =>
 		res.json()
